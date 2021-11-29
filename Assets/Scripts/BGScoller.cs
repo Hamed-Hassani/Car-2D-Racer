309cab8b2c11a8e8dc72ee523cbca52c088f1f92
hamed3949 <hamed2008ir@gmail.com>
2021-11-29 10:14:44 +0330

    add to github

---
+++ Assets/Scripts/BGScoller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScoller : MonoBehaviour
+{
+    private Renderer r;
+    public float speed = 0.5f;
+    private Vector2 offset;
+
+    private void Awake()
+    {
+        r = GetComponent<Renderer>();
+    }
+
+
+
+
+   
+    void Update()
+    {
+        offset = new Vector2(0f, Time.time * speed);
+        r.material.mainTextureOffset = offset;
+    }
+}
---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioSource takeOff, acceleration;
+
+    void Start()
+    {
+        
+    }
+
+    void TurnOnAcceleration()
+    {
+        takeOff.Stop();
+        acceleration.Play();
+    }
+}
